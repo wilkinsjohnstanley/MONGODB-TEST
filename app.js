f4f2c8dcb47c6bb7eb6387a6c7d83f6c3196b9f8
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-05 13:55:31 -0600

    y

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
 //ctrl + z to undo
 //initilize app & middleware
 const app = express()
+// db connection
+let db
 
-//localhost:3000
-app.listen(3000, ()=> {
-console.log('app is listening on port 3000')
+
+connectToDb((err)=>{
+    if(!err){
+    app.listen(3000, () => {
+     console.log('app is listening on port 3000')
+    })
+    db = getDb()
+    }
 })
+
 //routes
 app.get('/books', (req, res) => {
+    let books = []
+    db.collection('books')
+        .find() //cursor toArray fetches documents into array, forEach lets us process them individually
+        .sort({author:1})
+        .forEach(book=>books.push(book))
+        .then(()=>{
+            res.status(200).json(books)
+        })
+        .catch(()=>{
+            res.status(500).json({error: 'Could not fetch the documents'})
+        })
     res.json({mssg:"welcome to the api"})
 })
